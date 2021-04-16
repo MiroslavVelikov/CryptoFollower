a23aa30c14d137b0657a9562cf873978fd792a5d
TrayanShterionov <soccer359001@abv.bg>
2021-04-16 15:48:02 +0300

    Adding FindCurrency option
    Adding a option to find crypto currency.

---
+++ findCurrency.sh
@@ -0,0 +1,18 @@
+#Finding currency in coincapmarket
+
+echo "What crypto currency to find from CoinCapMarket?"
+read input
+name="name.txt"
+echo $input | tr "[A-Z]" "[a-z]" | tr " " "-" > $name
+clear
+./crFollower.sh $(cat $name)
+cat output.txt 
+echo "Do you want to make another search?[Y/N]"
+read answer
+if [ "$answer" == "Y" ]
+then 
+	./findCurrency.sh
+else 
+	clear
+	./menu.sh
+fi

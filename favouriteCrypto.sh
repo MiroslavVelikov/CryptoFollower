1ea57aea1e2b3f024054abadf4dc3ecdd79800c5
TrayanShterionov <soccer359001@abv.bg>
2021-04-17 16:01:22 +0300

    Adding Favour

---
+++ favouriteCrypto.sh
@@ -0,0 +1,18 @@
+Favour="Favour.txt"
+echo "$(cat $Favour)" | tr "[A-Z]" "[a-z]" | tr " " "-" > names.txt
+
+
+readarray -td, array <<< "$(cat names.txt),"; unset 'array[-1]'
+
+echo "Currency--Price--Price_Change"
+
+for name in "${array[@]}"
+do
+
+	./crFollower.sh $name
+	grep -o '\$[0-9,.-]*' output.txt | sed '1,2!d' > values.txt
+	price="$(echo $(cat values.txt) | cut -d' ' -f1)"
+	price_change="$(echo $(cat values.txt) | cut -d' ' -f2)"
+	printf "${name}--${price}--${price_change}"
+	echo  
+done
